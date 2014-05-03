c4b8573715fe26ac1de1b586acd4af8a6aa13f7a
Luke Orland <lorland@neverware.com>
2014-05-03 19:14:03 -0400

    add extra.vimrc and after.vimrc with custom configs

--- install.sh
+++ install.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-mkdir -p ~/src
-cd ~/src
-
-
 # Vim
+INSTALLDIR=deps/vimified
+create_symlinks () {
+    if [ ! -f ~/.vim ]; then
+        echo "Now, we will create ~/.vim and ~/.vimrc files to configure Vim."
+        ln -sfn $INSTALLDIR ~/.vim
+    fi
+
+    if [ ! -f ~/.vimrc ]; then
+        ln -sfn $INSTALLDIR/vimrc ~/.vimrc
+    fi
+  }
+
+create_symlinks
+ln -sfn $PWD/vimrc.extra ~/.vim/extra.vimrc
+ln -sfn $PWD/vimrc.after ~/.vim/after.vimrc
+echo "let g:vimified_packages = ['general', 'fancy', 'js', 'os', 'html', 'python', 'coding', 'color']" > ~/.vim/local.vimrc
+pushd $PWD
 cd deps
 sh vimified/install.sh
-echo "let g:vimified_packages = ['general', 'fancy', 'js', 'os', 'html', 'python', 'coding', 'color']" > ~/.vim/local.vimrc
-cd -
+popd
 
 # Git
-ln -s $PWD/deps/yadr/git/gitconfig ~/.gitconfig
-ln -s $PWD/gitconfig.neverware ~/.gitconfig.user
-ln -s $PWD/deps/yadr/git/gitignore ~/.gitignore
+ln -sfn $PWD/deps/yadr/git/gitconfig ~/.gitconfig
+ln -sfn $PWD/gitconfig.neverware ~/.gitconfig.user
+ln -sfn $PWD/deps/yadr/git/gitignore ~/.gitignore
