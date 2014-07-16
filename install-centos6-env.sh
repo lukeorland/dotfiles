efcafa66d7aa321fbea3ebde4647e0762384ee27
Luke Orland <lorland@neverware.com>
2014-07-16 12:24:14 -0400

    python vim stuff for work

--- install-centos6-env.sh
+++ install-centos6-env.sh
@@ -12,7 +12,10 @@ sudo rpm -i cmake-2.8.8-1.el6.rfx.x86_64.rpm
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar -xjf vim-7.4.tar.bz2
 cd vim74
-
 ./configure --prefix=$PREFIX --with-features=huge --enable-cscope --enable-rubyinterp --enable-pythoninterp --enable-luainterp=yes
-
 make && make install
+
+sudo pip install flake8 pylint
+
+# install dotvim
+# then install YouCompleteMe
