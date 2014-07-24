9405cc51a30ae518d61d5fd94297c07f2ea5327f
Luke Orland <lorland@neverware.com>
2014-07-24 17:12:22 -0400

    .profile with that reconnects to ssh-agent between screen sessions

--- install-work.sh
+++ install-work.sh
@@ -13,3 +13,4 @@ ln -sfn $PWD/dotvim/deps/yadr/git/gitconfig ~/.gitconfig
 ln -sfn $PWD/dotvim/deps/yadr/git/gitignore ~/.gitignore
 ln -sfn $PWD/gitconfig.neverware ~/.gitconfig.user
 ln -sfn $PWD/screenrc ~/.screenrc
+ln -sfn $PWD/remote-server/dotprofile ~/.profile
