95d4180a78f03599fe8f34093e4d374af7178cf7
Luke Orland <lorland@neverware.com>
2014-08-07 20:01:25 -0400

    revise centos6 vim build

--- install-centos6-env.sh
+++ install-centos6-env.sh
@@ -12,8 +12,20 @@ sudo rpm -i cmake-2.8.8-1.el6.rfx.x86_64.rpm
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar -xjf vim-7.4.tar.bz2
 cd vim74
-./configure --prefix=$PREFIX --with-features=huge --enable-cscope --enable-rubyinterp --enable-pythoninterp --enable-luainterp=yes
-make && make install
+./configure \
+            --prefix=$PREFIX \
+            --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/local/lib/python2.7/config \
+            --enable-luainterp \
+            --enable-cscope \
+            #--enable-perlinterp \
+            #--enable-gui=gtk2
+
+make VIMRUNTIMEDIR=$PREFIX/share/vim/vim74
+make install
 
 sudo pip install flake8 pylint
 
